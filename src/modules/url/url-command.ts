cadd87d2890bea97d2f0f8352f9db8d9fdc81eb0
Patrique Legault <patrique.legault@gmail.com>
2022-08-20 14:38:23 -0400

    Adding additional urls

--- src/modules/url/url-command.ts
+++ src/modules/url/url-command.ts
@@ -130,7 +130,133 @@ export default class UrlCommand extends BaseCommand<BaseEvent> {
                 this.validateClientlibs(options)
             })
 
+        program.command('system:memoryusage')
+            .alias('smem')
+            .option("-o, --open")
+            .action((options) => {
+                this.systemMemoryUsage(options)
+            })
+
+        program.command('system:jstack-threads')
+            .alias('sthreads')
+            .option("-o, --open")
+            .action((options) => {
+                this.systemJstackThreads(options)
+            })
+
+        program.command('system:sling-properties')
+            .alias('sprops')
+            .option("-o, --open")
+            .action((options) => {
+                this.systemSlingProps(options)
+            })
+
+        program.command('system:sling-scheduler')
+            .alias('sched')
+            .option("-o, --open")
+            .action((options) => {
+                this.systemSlingScheduler(options)
+            })
+
+        program.command('system:query-performance')
+            .alias('queryperf')
+            .option("-o, --open")
+            .action((options) => {
+                this.systemQueryPerformance(options)
+            })
+
         return program
+    }
+
+    systemQueryPerformance(options: any): BaseEvent {
+        try {
+            const server: ServerInfo = ConfigLoader.get().get()
+            if (options.open) {
+                open(`${server.serverUrl}/libs/granite/operations/content/diagnosistools/queryPerformance.html`)
+            } else {
+                console.log(chalk.green(`${server.serverUrl}/libs/granite/operations/content/diagnosistools/queryPerformance.html`))
+            }
+
+            this.eventEmitter.emit('url', { command: 'system:query-performance', msg: 'Opened Browser Window', program: 'url', state: CommandState.SUCCEEDED } as CommandEvent)
+        } catch (e) {
+            this.eventEmitter.emit('url', { command: 'system:query-performance', msg: 'Failed to open browser window', program: 'url', state: CommandState.FAILED } as CommandEvent)
+            return this.eventEmitter
+        }
+
+        return this.eventEmitter
+    }
+
+
+    systemSlingScheduler(options: any): BaseEvent {
+        try {
+            const server: ServerInfo = ConfigLoader.get().get()
+            if (options.open) {
+                open(`${server.serverUrl}/system/console/status-slingscheduler`)
+            } else {
+                console.log(chalk.green(`${server.serverUrl}/system/console/status-slingscheduler`))
+            }
+
+            this.eventEmitter.emit('url', { command: 'system:sling-scheduler', msg: 'Opened Browser Window', program: 'url', state: CommandState.SUCCEEDED } as CommandEvent)
+        } catch (e) {
+            this.eventEmitter.emit('url', { command: 'system:sling-scheduler', msg: 'Failed to open browser window', program: 'url', state: CommandState.FAILED } as CommandEvent)
+            return this.eventEmitter
+        }
+
+        return this.eventEmitter
+    }
+
+    systemSlingProps(options: any): BaseEvent {
+        try {
+            const server: ServerInfo = ConfigLoader.get().get()
+            if (options.open) {
+                open(`${server.serverUrl}/system/console/status-slingprops`)
+            } else {
+                console.log(chalk.green(`${server.serverUrl}/system/console/status-slingprops`))
+            }
+
+            this.eventEmitter.emit('url', { command: 'system:sling-properties', msg: 'Opened Browser Window', program: 'url', state: CommandState.SUCCEEDED } as CommandEvent)
+        } catch (e) {
+            this.eventEmitter.emit('url', { command: 'system:sling-properties', msg: 'Failed to open browser window', program: 'url', state: CommandState.FAILED } as CommandEvent)
+            return this.eventEmitter
+        }
+
+        return this.eventEmitter
+    }
+
+    systemJstackThreads(options: any): BaseEvent {
+        try {
+            const server: ServerInfo = ConfigLoader.get().get()
+            if (options.open) {
+                open(`${server.serverUrl}/system/console/status-jstack-threaddump`)
+            } else {
+                console.log(chalk.green(`${server.serverUrl}/system/console/status-jstack-threaddump`))
+            }
+
+            this.eventEmitter.emit('url', { command: 'system:jstack-threads', msg: 'Opened Browser Window', program: 'url', state: CommandState.SUCCEEDED } as CommandEvent)
+        } catch (e) {
+            this.eventEmitter.emit('url', { command: 'system:jstack-threads', msg: 'Failed to open browser window', program: 'url', state: CommandState.FAILED } as CommandEvent)
+            return this.eventEmitter
+        }
+
+        return this.eventEmitter
+    }
+
+    systemMemoryUsage(options: any): BaseEvent {
+        try {
+            const server: ServerInfo = ConfigLoader.get().get()
+            if (options.open) {
+                open(`${server.serverUrl}/system/console/memoryusage`)
+            } else {
+                console.log(chalk.green(`${server.serverUrl}/system/console/memoryusage`))
+            }
+
+            this.eventEmitter.emit('url', { command: 'system:memoryusage', msg: 'Opened Browser Window', program: 'url', state: CommandState.SUCCEEDED } as CommandEvent)
+        } catch (e) {
+            this.eventEmitter.emit('url', { command: 'system:memoryusage', msg: 'Failed to open browser window', program: 'url', state: CommandState.FAILED } as CommandEvent)
+            return this.eventEmitter
+        }
+
+        return this.eventEmitter
     }
 
     validateClientlibs(options: any): BaseEvent {
