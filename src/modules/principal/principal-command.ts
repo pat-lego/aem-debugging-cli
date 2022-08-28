b61fedbf9346706a9d46baa0070ac13011464625
Patrique Legault <patrique.legault@gmail.com>
2022-08-28 19:38:10 -0400

    Creating group

--- src/modules/principal/principal-command.ts
+++ src/modules/principal/principal-command.ts
@@ -2,7 +2,7 @@ import { Command, Option } from "commander"
 import FormData from 'form-data'
 
 import BaseCommand from "../base-command.js"
-import BaseEvent from "../base-event.js"
+import BaseEvent, { CommandEvent, CommandState } from "../base-event.js"
 import httpclient from '../../utils/http.js'
 import { ServerInfo } from "../config/authentication/server-authentication.js"
 import ConfigLoader from "../config/config-loader.js"
@@ -26,12 +26,41 @@ export default class PrincipalCommand extends BaseCommand<BaseEvent> {
             .action((username: string, authorizableId: string, password: string, options: any) => {
                 this.createUser(username, authorizableId, password, options)
             })
+
+        program.command('create:group')
+            .alias('cugrp')
+            .argument('<name>', 'The users name')
+            .argument('<authorizableId>', 'The ID of the user')
+            .action((name: string, authorizableId: string) => {
+                this.createGroup(name, authorizableId)
+            })
         return program
+    }
+
+    createGroup(name: string, authorizableId: string) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+
+        let formData = new FormData()
+        formData.append('createGroup', name)
+        formData.append('authorizableId', authorizableId)
+
+        httpclient.post({ serverInfo: serverInfo, path: '/libs/granite/security/post/authorizables', body: formData, headers: { 'Content-Type': 'multipart/form-data' } }).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data)
+                this.eventEmitter.emit(this.name, { command: 'create:group', program: this.name, msg: `Successfully create group with name ${name}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } else {
+                console.log(`Failed to create group with name ${name} with http error code ${response.status}`)
+                this.eventEmitter.emit(this.name, { command: 'create:group', program: this.name, msg: `Failed to create group with name ${name} with http error code ${response.status}`, state: CommandState.FAILED } as CommandEvent)
+            }
+        }).catch((error) => {
+            console.error(`Failed to create group with name ${name} with http error ${error}`)
+            this.eventEmitter.emit(this.name, { command: 'create:group', program: this.name, msg: `Failed to create group with name ${name} with http error ${error}`, state: CommandState.FAILED } as CommandEvent)
+        })
     }
 
     createUser(username: string, authorizableId: string, password: string, options: any) {
         const serverInfo: ServerInfo = ConfigLoader.get().get()
-        
+
         let formData = new FormData()
         formData.append('createUser', username)
         formData.append('authorizableId', authorizableId)
@@ -39,16 +68,23 @@ export default class PrincipalCommand extends BaseCommand<BaseEvent> {
 
         formData = this.appendOptionsForCreateUser(formData, options.profile)
 
-        httpclient.post({serverInfo: serverInfo, path: '/libs/granite/security/post/authorizables', body: formData, headers: {'Content-Type': 'multipart/form-data'}}).then((response) => {
-            console.log(response.data)
+        httpclient.post({ serverInfo: serverInfo, path: '/libs/granite/security/post/authorizables', body: formData, headers: { 'Content-Type': 'multipart/form-data' } }).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data)
+                this.eventEmitter.emit(this.name, { command: 'create:user', program: this.name, msg: `Successfully create user with name ${username}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } else {
+                console.log(`Failed to create user with name ${username} with http error code ${response.status}`)
+                this.eventEmitter.emit(this.name, { command: 'create:user', program: this.name, msg: `Failed to create user with name ${username} with http error code ${response.status}`, state: CommandState.FAILED } as CommandEvent)
+            }
         }).catch((error) => {
-            console.log(error)
+            console.error(`Failed to create user with name ${username} with http error ${error}`)
+            this.eventEmitter.emit(this.name, { command: 'create:user', program: this.name, msg: `Failed to create user with name ${username} with http error ${error}`, state: CommandState.FAILED } as CommandEvent)
         })
     }
 
     appendOptionsForCreateUser(formData: FormData, profile: string[]): FormData {
         if (profile) {
-            for(const option of profile) {
+            for (const option of profile) {
                 const anOption: string[] = option.split('=')
 
                 if (anOption.length === 2) {
