24da616a63f7f87140416a4a98736dcee80659ab
Patrique Legault <patrique.legault@gmail.com>
2022-08-30 17:15:12 -0400

    Adding assets api

--- src/modules/assets/assets-command.ts
+++ src/modules/assets/assets-command.ts
@@ -37,11 +37,126 @@ export default class AssetsCommand extends BaseCommand<BaseEvent> {
                 this.createAsset(name, path, options)
             })
 
+        program.command('update:asset')
+            .argument('<name>', 'The name of the asset you want to update')
+            .argument('<path>', 'The path to the asset on disk')
+            .addOption(new Option('-c, --content-type <value>', 'The content type of the asset').default('image/png'))
+            .action((name: string, path: string, options: any) => {
+                this.updateAsset(name, path, options)
+            })
+
+        program.command('update:asset-metadata')
+            .argument('<path>', 'The path to the asset on disk')
+            .addOption(new Option('-p, --properties <props...>', 'The properties you want to update in the asset'))
+            .action((path: string, options: any) => {
+                this.updateAssetMetadata(path, options)
+            })
+
+        program.command('delete:asset')
+            .argument('<path>', 'The path of the asset to delete')
+            .action((path: string) => {
+                this.deleteAsset(path)
+            })
+
+        program.command('move:asset')
+            .argument('<oldpath>', 'The old path of the asset to move')
+            .argument('<newpath>', 'The new path of the asset to move')
+            .addOption(new Option('-d, --depth <depth>', 'The recursion depth to apply').choices(['0', 'infinity']).default('0'))
+            .addOption(new Option('-o, --overwrite <overwrite>', 'Either to overwrite (T) or preserve (F) existing assets').choices(['T', 'F']).default('F'))
+            .action((oldpath: string, newpath: string, options: any) => {
+                this.moveAsset(oldpath, newpath, options)
+            })
+
+        program.command('copy:asset')
+            .argument('<oldpath>', 'The old path of the asset to move')
+            .argument('<newpath>', 'The new path of the asset to move')
+            .addOption(new Option('-d, --depth <depth>', 'The recursion depth to apply').choices(['0', 'infinity']).default('0'))
+            .addOption(new Option('-o, --overwrite <overwrite>', 'Either to overwrite (T) or preserve (F) existing assets').choices(['T', 'F']).default('F'))
+            .action((oldpath: string, newpath: string, options: any) => {
+                this.copyAsset(oldpath, newpath, options)
+            })
+
 
         return program
     }
 
-    createAsset(name: string, path: string, options:any) {
+    deleteAsset(path: string) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+
+
+        httpclient.dlt({ serverInfo: serverInfo, path: `/api/assets/${path}` }).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data)
+
+                this.eventEmitter.emit(this.name, { command: 'delete:asset', program: this.name, msg: `Successfully deleted asset at path ${path}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } else {
+                console.log(`Failed to delete asset at path ${path} with http error code ${response.status}`)
+
+                this.eventEmitter.emit(this.name, { command: 'delete:asset', program: this.name, msg: `Failed to delete asset at path ${path} with http error code ${response.status}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            }
+
+        }).catch((e: Error) => {
+            console.error(`Failed to delete asset at path ${path} with error ${e}`)
+
+            this.eventEmitter.emit(this.name, { command: 'delete:asset', program: this.name, msg: `Failed to delete asset at path ${path} with error ${e}`, state: CommandState.SUCCEEDED } as CommandEvent)
+        })
+    }
+
+    moveAsset(oldpath: string, newpath: string, options: any) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+
+        const headers: { [key: string]: string } = {
+            'X-Depth': options.depth,
+            'X-Overwrite': options.overwrite,
+            'X-Destination': new URL(newpath).toString()
+        }
+
+        httpclient.move({ serverInfo: serverInfo, path: `/api/assets/${oldpath}`, headers: headers }).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data)
+
+                this.eventEmitter.emit(this.name, { command: 'move:asset', program: this.name, msg: `Successfully moved asset at path ${oldpath}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } else {
+                console.log(`Failed to move asset at path ${oldpath} with http error code ${response.status}`)
+
+                this.eventEmitter.emit(this.name, { command: 'move:asset', program: this.name, msg: `Failed to move asset at path ${oldpath} with http error code ${response.status}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            }
+
+        }).catch((e: Error) => {
+            console.error(`Failed to move asset at path ${oldpath} with error ${e}`)
+
+            this.eventEmitter.emit(this.name, { command: 'move:asset', program: this.name, msg: `Failed to move asset at path ${oldpath} with error ${e}`, state: CommandState.SUCCEEDED } as CommandEvent)
+        })
+    }
+
+    copyAsset(oldpath: string, newpath: string, options: any) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+
+        const headers: { [key: string]: string } = {
+            'X-Depth': options.depth,
+            'X-Overwrite': options.overwrite,
+            'X-Destination': new URL(newpath).toString()
+        }
+
+        httpclient.copy({ serverInfo: serverInfo, path: `/api/assets/${oldpath}`, headers: headers }).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data)
+
+                this.eventEmitter.emit(this.name, { command: 'copy:asset', program: this.name, msg: `Successfully copied asset at path ${oldpath}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } else {
+                console.log(`Failed to copy asset at path ${oldpath} with http error code ${response.status}`)
+
+                this.eventEmitter.emit(this.name, { command: 'copy:asset', program: this.name, msg: `Failed to copy asset at path ${oldpath} with http error code ${response.status}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            }
+
+        }).catch((e: Error) => {
+            console.error(`Failed to copy asset at path ${oldpath} with error ${e}`)
+
+            this.eventEmitter.emit(this.name, { command: 'copy:asset', program: this.name, msg: `Failed to copy asset at path ${oldpath} with error ${e}`, state: CommandState.SUCCEEDED } as CommandEvent)
+        })
+    }
+
+    createAsset(name: string, path: string, options: any) {
         const serverInfo: ServerInfo = ConfigLoader.get().get()
         const formData: FormData = new FormData()
         formData.append('file', fs.createReadStream(path))
@@ -63,6 +178,78 @@ export default class AssetsCommand extends BaseCommand<BaseEvent> {
 
             this.eventEmitter.emit(this.name, { command: 'create:asset', program: this.name, msg: `Failed to create asset ${name} with error  ${e}`, state: CommandState.SUCCEEDED } as CommandEvent)
         })
+    }
+
+    updateAsset(name: string, path: string, options: any) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+        const formData: FormData = new FormData()
+        formData.append('file', fs.createReadStream(path))
+        formData.append('Content-Type', options.contentType)
+
+        httpclient.put({ serverInfo: serverInfo, path: `/api/assets/${name}`, body: formData, headers: { 'Content-Type': 'multipart/form-data' } }).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data)
+
+                this.eventEmitter.emit(this.name, { command: 'update:asset', program: this.name, msg: `Successfully updated asset content ${name}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } else {
+                console.log(`Failed to update asset ${name} with http error code ${response.status}`)
+
+                this.eventEmitter.emit(this.name, { command: 'update:asset', program: this.name, msg: `Failed to update asset ${name} with http error code ${response.status}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            }
+
+        }).catch((e: Error) => {
+            console.error(`Failed to update asset ${name} with error  ${e}`)
+
+            this.eventEmitter.emit(this.name, { command: 'update:asset', program: this.name, msg: `Failed to update asset ${name} with error  ${e}`, state: CommandState.SUCCEEDED } as CommandEvent)
+        })
+    }
+
+    updateAssetMetadata(path: string, options: any) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+
+        httpclient.put({ serverInfo: serverInfo, path: `/api/assets/${path}`, body: { class: 'asset', properties: this.createAssetMetadataJson(options.properties) }, headers: { 'Content-Type': 'application/json' } }).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data)
+
+                this.eventEmitter.emit(this.name, { command: 'update:asset-metadata', program: this.name, msg: `Successfully updated asset metadata at path ${path}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } else {
+                console.log(`Failed to updated asset metadata at path ${path} with http error code ${response.status}`)
+
+                this.eventEmitter.emit(this.name, { command: 'update:asset-metadata', program: this.name, msg: `Failed to update asset metadata at path ${path} with http error code ${response.status}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            }
+
+        }).catch((e: Error) => {
+            console.error(`Failed to update asset metadata at path ${path} with error  ${e}`)
+
+            this.eventEmitter.emit(this.name, { command: 'update:asset-metadata', program: this.name, msg: `Failed to update asset metadata at path ${path} with error  ${e}`, state: CommandState.SUCCEEDED } as CommandEvent)
+        })
+    }
+
+    createAssetMetadataJson(properties: string[]): { [key: string]: string } {
+        const result: { [key: string]: string } = {}
+
+        if (properties.length === 0) {
+            throw Error('No properties provided when trying to create folder for assets')
+        }
+
+        let hasJcrTitle = false
+        for (const props of properties) {
+            const prop: string[] = props.split('=')
+
+            if (prop.length === 2) {
+                if (prop[0] === 'jcr:title') {
+                    hasJcrTitle = true
+                }
+                result[prop[0]] = prop[1]
+            }
+        }
+
+        if (hasJcrTitle === false) {
+            throw Error('Missing jcr:title in the create folder API call')
+        }
+
+        return result
+
     }
 
     createFolder(folder: string, options: any) {
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 import { Authentication, ServerInfo } from "../modules/config/authentication/server-authentication.js"
 
-
 const get = async (request: { serverInfo: ServerInfo, path: string, params?: any, headers?: any }): Promise<any> => {
     if (request.serverInfo.type === Authentication.BASIC) {
         return basicGet(request)
@@ -33,6 +32,40 @@ const noneGet = async (request: { serverInfo: ServerInfo, path: string, params?:
         return resp
     } catch (e) {
         throw new Error(`Could not perform http get to ${request.serverInfo.serverUrl}${request.path} - ${e}`)
+    }
+}
+
+const dlt = async (request: { serverInfo: ServerInfo, path: string, params?: any, headers?: any }): Promise<any> => {
+    if (request.serverInfo.type === Authentication.BASIC) {
+        return basicDelete(request)
+    }
+
+    if (request.serverInfo.type === Authentication.NONE) {
+        return noneDelete(request)
+    }
+    throw new Error('Could not identify valid Authentication type for request')
+}
+
+const basicDelete = async (request: { serverInfo: ServerInfo, path: string, params?: any, headers?: any }): Promise<any> => {
+    if (request.headers) {
+        request.headers = { ...request.headers, 'Authorization': `Basic ${request.serverInfo.auth}` }
+    } else {
+        request.headers = { 'Authorization': `Basic ${request.serverInfo.auth}` }
+    }
+    try {
+        const resp: any = await axios.delete(`${request.serverInfo.serverUrl}${request.path}`, { headers: request.headers, params: request.params })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http delete to ${request.serverInfo.serverUrl}${request.path} - ${e}`)
+    }
+}
+
+const noneDelete = async (request: { serverInfo: ServerInfo, path: string, params?: any, headers?: any }): Promise<any> => {
+    try {
+        const resp: any = await axios.delete(`${request.serverInfo.serverUrl}${request.path}`, { headers: request.headers, params: request.params })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http delete to ${request.serverInfo.serverUrl}${request.path} - ${e}`)
     }
 }
 
@@ -81,5 +114,136 @@ const nonePost = async (request: { serverInfo: ServerInfo, path: string, body?: 
     }
 }
 
+const put = async (request: { serverInfo: ServerInfo, path: string, body: any, headers?: any }): Promise<any> => {
+    if (request.serverInfo.type === Authentication.BASIC) {
+        return basicPut(request)
+    }
 
-export default { get, post }
+    if (request.serverInfo.type === Authentication.NONE) {
+        return nonePut(request)
+    }
+    throw new Error('Could not identify valid Authentication type for request')
+}
+
+const basicPut = async (request: { serverInfo: ServerInfo, path: string, body?: any, headers?: any }): Promise<any> => {
+    try {
+        let localHeaders = {
+            'Authorization': `Basic ${request.serverInfo.auth}`
+        }
+        if (request.headers) {
+            localHeaders = { ...localHeaders, ...request.headers }
+        }
+        const resp: any = await axios({
+            method: 'put',
+            url: `${request.serverInfo.serverUrl}${request.path}`,
+            data: request.body,
+            headers: localHeaders
+        })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http put to ${request.serverInfo.serverUrl}${request.path}  - ${e}`)
+    }
+}
+
+const nonePut = async (request: { serverInfo: ServerInfo, path: string, body?: any, headers?: any }): Promise<any> => {
+    try {
+        const resp: any = await axios({
+            method: 'put',
+            url: `${request.serverInfo.serverUrl}${request.path}`,
+            data: request.body,
+            headers: request.headers
+        })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http put to ${request.serverInfo.serverUrl}${request.path} - ${e}`)
+    }
+}
+
+const move = async (request: { serverInfo: ServerInfo, path: string, headers?: any }): Promise<any> => {
+    if (request.serverInfo.type === Authentication.BASIC) {
+        return basicMove(request)
+    }
+
+    if (request.serverInfo.type === Authentication.NONE) {
+        return noneMove(request)
+    }
+    throw new Error('Could not identify valid Authentication type for request')
+}
+
+const basicMove = async (request: { serverInfo: ServerInfo, path: string, body?: any, headers?: any }): Promise<any> => {
+    try {
+        let localHeaders = {
+            'Authorization': `Basic ${request.serverInfo.auth}`
+        }
+        if (request.headers) {
+            localHeaders = { ...localHeaders, ...request.headers }
+        }
+        const resp: any = await axios({
+            method: 'move',
+            url: `${request.serverInfo.serverUrl}${request.path}`,
+            headers: localHeaders
+        })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http put to ${request.serverInfo.serverUrl}${request.path}  - ${e}`)
+    }
+}
+
+const noneMove = async (request: { serverInfo: ServerInfo, path: string, headers?: any }): Promise<any> => {
+    try {
+        const resp: any = await axios({
+            method: 'move',
+            url: `${request.serverInfo.serverUrl}${request.path}`,
+            headers: request.headers
+        })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http put to ${request.serverInfo.serverUrl}${request.path} - ${e}`)
+    }
+}
+
+const copy = async (request: { serverInfo: ServerInfo, path: string, headers?: any }): Promise<any> => {
+    if (request.serverInfo.type === Authentication.BASIC) {
+        return basicCopy(request)
+    }
+
+    if (request.serverInfo.type === Authentication.NONE) {
+        return noneCopy(request)
+    }
+    throw new Error('Could not identify valid Authentication type for request')
+}
+
+const basicCopy = async (request: { serverInfo: ServerInfo, path: string, body?: any, headers?: any }): Promise<any> => {
+    try {
+        let localHeaders = {
+            'Authorization': `Basic ${request.serverInfo.auth}`
+        }
+        if (request.headers) {
+            localHeaders = { ...localHeaders, ...request.headers }
+        }
+        const resp: any = await axios({
+            method: 'copy',
+            url: `${request.serverInfo.serverUrl}${request.path}`,
+            headers: localHeaders
+        })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http put to ${request.serverInfo.serverUrl}${request.path}  - ${e}`)
+    }
+}
+
+const noneCopy = async (request: { serverInfo: ServerInfo, path: string, headers?: any }): Promise<any> => {
+    try {
+        const resp: any = await axios({
+            method: 'copy',
+            url: `${request.serverInfo.serverUrl}${request.path}`,
+            headers: request.headers
+        })
+        return resp
+    } catch (e) {
+        throw new Error(`Could not perform http put to ${request.serverInfo.serverUrl}${request.path} - ${e}`)
+    }
+}
+
+
+export default { get, post, dlt, put, move, copy }
