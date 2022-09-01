e2444c3d75c86aee59a3a3b957844a86ee6f2411
Patrique Legault <patrique.legault@gmail.com>
2022-09-01 15:49:03 -0400

    Adding distribution commands

--- src/modules/deployment/deployment-command.ts
+++ src/modules/deployment/deployment-command.ts
@@ -33,6 +33,31 @@ export default class DeploymentCommand extends BaseCommand<BaseEvent> {
                 this.distributionConfig(agentid)
             })
 
+        program.command('distribution:add')
+            .alias('da')
+            .addArgument(new Argument('<agentid>', 'The distribution agent id'))
+            .addArgument(new Argument('<path>', 'The path to add to the agent id'))
+            .action((agentid: string, path: string) => {
+                this.distributionAdd(agentid, path)
+            })
+
+        program.command('distribution:clear-queue')
+            .alias('da')
+            .addArgument(new Argument('<agentid>', 'The distribution agent id'))
+            .addArgument(new Argument('<queue>', 'The name of the queue'))
+            .addOption(new Option('-l, --limit', 'The amount to clear').default('-1'))
+            .action((agentid: string, queue: string, options: any) => {
+                this.distributionClear(agentid, queue, options)
+            })
+
+        program.command('distribution:pull')
+            .alias('dp')
+            .addArgument(new Argument('<agentid>', 'The distribution agent id'))
+            .addArgument(new Argument('<path>', 'The path to pull from the agent id'))
+            .action((agentid: string, path: string) => {
+                this.distributionPull(agentid, path)
+            })
+
         program.command('replication:pause')
             .alias('rp')
             .description('Running this command twice will resume the queue')
@@ -346,6 +371,69 @@ export default class DeploymentCommand extends BaseCommand<BaseEvent> {
             }).catch((error) => {
                 console.error(`Failed to retrieve the agent config for ${agentid} due to the following error ${error}`)
                 this.eventEmitter.emit(this.name, { command: 'distribution:config', program: this.name, msg: `Failed to retrieve the agent config for ${agentid} due to the following error ${error}`, state: CommandState.FAILED } as CommandEvent)
+            })
+    }
+
+    distributionAdd(agentid: string, path: string) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+        const formData = new FormData()
+        formData.append("action", "ADD")
+        formData.append("path", path)
+
+        httpclient.post({ serverInfo: serverInfo, path: `/libs/sling/distribution/services/agents/${agentid}`, body: formData, headers: { 'Content-Type': 'multipart/form-data' } })
+            .then((response) => {
+                if (response.status >= 200 && response.status < 300) {
+                    console.log(response.data)
+                    this.eventEmitter.emit(this.name, { command: 'distribution:add', program: this.name, msg: `Successfully added path ${path} to the agent ${agentid}`, state: CommandState.SUCCEEDED } as CommandEvent)
+                } else {
+                    console.log(`Failed to add path ${path} to the agent ${agentid}`)
+                    this.eventEmitter.emit(this.name, { command: 'distribution:add', program: this.name, msg: `Failed to add path ${path} to the agent ${agentid} with http error code ${response.status}`, state: CommandState.FAILED } as CommandEvent)
+                }
+            }).catch((error) => {
+                console.error(`Failed to add path ${path} to the agent ${agentid} due to the following error ${error}`)
+                this.eventEmitter.emit(this.name, { command: 'distribution:add', program: this.name, msg: `Failed to add path ${path} to the agent ${agentid} due to the following error ${error}`, state: CommandState.FAILED } as CommandEvent)
+            })
+    }
+
+    distributionClear(agentid: string, queue: string, options: any) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+        const formData = new FormData()
+        formData.append("operation", "delete")
+        formData.append("limit", options.limit)
+
+        httpclient.post({ serverInfo: serverInfo, path: `/libs/sling/distribution/services/agents/${agentid}/queues/${queue}`, body: formData, headers: { 'Content-Type': 'multipart/form-data' } })
+            .then((response) => {
+                if (response.status >= 200 && response.status < 300) {
+                    console.log(`Successfully cleared queue ${queue} on the agent ${agentid}`)
+                    this.eventEmitter.emit(this.name, { command: 'distribution:clear-queue', program: this.name, msg: `Successfully cleared queue ${queue} on the agent ${agentid}`, state: CommandState.SUCCEEDED } as CommandEvent)
+                } else {
+                    console.log(`Failed to clear queue ${queue} on the agent ${agentid}`)
+                    this.eventEmitter.emit(this.name, { command: 'distribution:clear-queue', program: this.name, msg:`Failed to clear queue ${queue} on the agent ${agentid} with http error code ${response.status}`, state: CommandState.FAILED } as CommandEvent)
+                }
+            }).catch((error) => {
+                console.error(`Failed to clear queue ${queue} on the agent ${agentid} due to the following error ${error}`)
+                this.eventEmitter.emit(this.name, { command: 'distribution:clear-queue', program: this.name, msg:`Failed to clear queue ${queue} on the agent ${agentid} due to the following error ${error}`, state: CommandState.FAILED } as CommandEvent)
+            })
+    }
+
+    distributionPull(agentid: string, path: string) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+        const formData = new FormData()
+        formData.append("action", "PULL")
+        formData.append("path", path)
+
+        httpclient.post({ serverInfo: serverInfo, path: `/libs/sling/distribution/services/agents/${agentid}`, body: formData, headers: { 'Content-Type': 'multipart/form-data' } })
+            .then((response) => {
+                if (response.status >= 200 && response.status < 300) {
+                    console.log(response.data)
+                    this.eventEmitter.emit(this.name, { command: 'distribution:pull', program: this.name, msg: `Successfully pulled path ${path} to the agent ${agentid}`, state: CommandState.SUCCEEDED } as CommandEvent)
+                } else {
+                    console.log(`Failed to pull path ${path} to the agent ${agentid}`)
+                    this.eventEmitter.emit(this.name, { command: 'distribution:pull', program: this.name, msg: `Failed to pull path ${path} to the agent ${agentid} with http error code ${response.status}`, state: CommandState.FAILED } as CommandEvent)
+                }
+            }).catch((error) => {
+                console.error(`Failed to pull path ${path} to the agent ${agentid} due to the following error ${error}`)
+                this.eventEmitter.emit(this.name, { command: 'distribution:pull', program: this.name, msg: `Failed to pull path ${path} to the agent ${agentid} due to the following error ${error}`, state: CommandState.FAILED } as CommandEvent)
             })
     }
 
