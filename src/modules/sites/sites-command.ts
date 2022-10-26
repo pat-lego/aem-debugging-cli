7cb68241a9854db9680b97bb1fb9becf6d7bb142
Patrique Legault <patrique.legault@gmail.com>
2022-10-26 09:05:06 -0400

    Adding dictionary JSON exporter

--- src/modules/sites/sites-command.ts
+++ src/modules/sites/sites-command.ts
@@ -4,6 +4,7 @@ import BaseEvent, { CommandEvent, CommandState } from "../base-event.js"
 import { ServerInfo } from "../config/authentication/server-authentication.js"
 import ConfigLoader from "../config/config-loader.js"
 import httpclient from '../../utils/http.js'
+import open from 'open'
 
 export default class SitesCommands extends BaseCommand<BaseEvent> {
     name: string = 'sites'
@@ -29,12 +30,19 @@ export default class SitesCommands extends BaseCommand<BaseEvent> {
                 this.showBluePrint(path)
             })
 
+        program.command('show:dictionary')
+            .alias('sdict')
+            .argument('<i18n>', 'The language code for the dictionary')
+            .action((path: string) => {
+                this.showDictionary(path)
+            })
+
         return program
     }
 
     showLiveCopy(path: string) {
         const serverInfo: ServerInfo = ConfigLoader.get().get()
-        
+
 
         httpclient.get({ serverInfo: serverInfo, path: `${path}.msm.json` }).then((response) => {
             if (response.status >= 200 && response.status < 300) {
@@ -53,7 +61,7 @@ export default class SitesCommands extends BaseCommand<BaseEvent> {
     showBluePrint(path: string) {
         const serverInfo: ServerInfo = ConfigLoader.get().get()
 
-        const params: {[key: string]: string} = {
+        const params: { [key: string]: string } = {
             'maxSize': '500',
             'advancedStatus': 'true',
             'returnRelationships': 'true',
@@ -72,6 +80,24 @@ export default class SitesCommands extends BaseCommand<BaseEvent> {
             console.log(`Failed to list the blueprint msm settings for the following page ${path} with error ${error}`)
             this.eventEmitter.emit(this.name, { command: 'show:blueprint', program: this.name, msg: `Failed to list the blueprint msm settings for the following page ${path} with error ${error}`, state: CommandState.SUCCEEDED } as CommandEvent)
         })
+    }
+
+    showDictionary(i18n: string) {
+        const serverInfo: ServerInfo = ConfigLoader.get().get()
+        if (i18n) {
+            try {
+                open(`${serverInfo.serverUrl}/libs/cq/i18n/dict.${i18n}.json`)
+                console.log(`Showcased the dictionary for ${i18n}`)
+                this.eventEmitter.emit(this.name, { command: 'show:dictionary', program: this.name, msg: `Succeeded to show the dictionary for locale ${i18n}`, state: CommandState.SUCCEEDED } as CommandEvent)
+            } catch (error) {
+                console.log(`Failed to show the dictionary for ${i18n} with error ${error}`)
+                this.eventEmitter.emit(this.name, { command: 'show:dictionary', program: this.name, msg: `Failed to show the dictionary for locale ${i18n} with error ${error}`, state: CommandState.FAILED } as CommandEvent)
+            }
+        } else {
+            console.log(`Failed to show the dictionary since the locale is not defined please provide a valid locale`)
+            this.eventEmitter.emit(this.name, { command: 'show:dictionary', program: this.name, msg: `Failed to show the dictionary since the locale is not defined please provide a valid locale`, state: CommandState.FAILED } as CommandEvent)
+        }
+
     }
 
 
