ebd27556444d0f24e7a3855e72aad9686e514057
Patrique Legault <patrique.legault@gmail.com>
2022-11-21 17:05:54 -0500

    Adding ability to ignore SSL cert verification

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ util.inspect.defaultOptions.maxArrayLength = null
 // Override maximum print depth to show all objects
 util.inspect.defaultOptions.depth = null
 
+// Ignore SSL certificate verification
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0'
+
 const main = new Command()
 
 for (let commands of Commands) {
